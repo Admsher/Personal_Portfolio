bf938ce8135c1570f12301f017b66ca147787611
Varad Shinde <99754991+Admsher@users.noreply.github.com>
2025-11-07 00:01:01 +0530

    FIx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import { BrowserRouter } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 const App = () => {
   const [isMobile, setIsMobile] = useState(false);
 
